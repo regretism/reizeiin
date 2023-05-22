b23461951b90831d7c1a8dc22f34790de5b9be31
regretism <85019980+regretism@users.noreply.github.com>
2023-05-22 21:59:59 +0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import Calendar from 'react-calendar';
-import 'react-calendar/dist/Calendar.css';
+import './Calendar.css';
 
 const FoodDiary = () => {
     const [selectedProduct, setSelectedProduct] = useState('');
